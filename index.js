e3c9a6418accb15d576b28ee690993c54f8d29ac
codeconcept <contact@codeconcept.fr>
2019-12-31 13:06:04 +0100

    Specifying encoding to get a string rather than a buffer

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
-fs.readFile("./technos.txt", (err, data) => {
+// spefiying encoding to get a string rather that a buffer
+fs.readFile("./technos.txt", "utf8", (err, data) => {
   console.log(data);
 });
