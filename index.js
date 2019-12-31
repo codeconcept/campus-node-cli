31971ffb5b3b4d1b813efed8e6769b15b14b34fc
codeconcept <contact@codeconcept.fr>
2019-12-31 20:14:09 +0100

    Creating a file using fs.createWriteStream

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
-const request = require("request");
 const fs = require("fs");
+const writer = fs.createWriteStream("./massif.txt");
 
-request("https://www.campus.academy/").pipe(fs.createWriteStream("codac.html"));
+for (let i = 0; i < 100; i++) {
+  writer.write(`${i} On est (quasi) en 2020\n`);
+}
+
+writer.on("data", function(chunk) {
+  console.log("chunk", chunk);
+});
+
+writer.end(function() {
+  console.log("end");
+});
+
+writer.on("close", function() {
+  console.log("closed");
+});
+
+writer.on("open", function() {
+  console.log("open");
+});
+
+console.log("First!");
